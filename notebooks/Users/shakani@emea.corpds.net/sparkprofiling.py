c02ae99201f1c8b89f1abcbb9537f4c58d093d05
kanishk sharma <40290753+kanishksha@users.noreply.github.com>
2020-12-31 10:54:02 +0100

    commit

--- notebooks/Users/shakani@emea.corpds.net/sparkprofiling.py
+++ notebooks/Users/shakani@emea.corpds.net/sparkprofiling.py
@@ -17,7 +17,7 @@ display(dbutils.fs.ls("/mnt/landing"))
 # COMMAND ----------
 
 # python
-df = spark.read.csv("/mnt/landing", inferSchema="True",header = "true")
+df = spark.read.csv("/mnt/landing/ll2.csv", inferSchema="True",header = "true")
 
 # COMMAND ----------
 
@@ -25,20 +25,30 @@ display(df)
 
 # COMMAND ----------
 
+df.count()
+
+# COMMAND ----------
+
+# selecting specific columns those are important and affects the data(selected dataframes)
+
+df_sel = df.select("term", "home_ownership", "installment", "grade", "purpose", "int_rate", "addr_state", "loan_status", "application_type", "loan_amnt", "emp_length", "annual_inc", "dti", "delinq_2yrs","revol_bal", "revol_util", "total_acc", "num_tl_90g_dpd_24m", "dti_joint")
+
+
+# COMMAND ----------
+
+df_sel.describe("term", "loan_amnt", "emp_length", "annual_inc", "dti", "delinq_2yrs","revol_bal", "revol_util", "total_acc").show()
+
+# COMMAND ----------
+
+df_sel.cache()
+
+# COMMAND ----------
+
 import spark_df_profiling
 
 # COMMAND ----------
 
-#df = spark.table('lc_loan_dataa')
-#display(df)
-
-# COMMAND ----------
-
-df.describe().show()
-
-# COMMAND ----------
-
-report = spark_df_profiling.ProfileReport(df)
+report = spark_df_profiling.ProfileReport(df_sel)
 
 # COMMAND ----------
 
